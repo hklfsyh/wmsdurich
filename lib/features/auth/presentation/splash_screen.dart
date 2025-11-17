fc796471d7088b663563d0cca7d73d9ab77aaaae
hklfsyh <haikalafifsyah@gmail.com>
2025-11-17 21:33:59 +0700

    Splash Screen

--- lib/features/auth/presentation/splash_screen.dart
+++ lib/features/auth/presentation/splash_screen.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:wms_durich/core/constants/asset_paths.dart';
+import 'package:wms_durich/core/theme/app_colors.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkAuthState();
+  }
+
+  void _checkAuthState() async {
+    await Future.delayed(const Duration(seconds: 10));
+
+    if (mounted) {
+      context.go('/login');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: AppColors.background,
       body: Center(
-        child: Text('DURICH WMS - Loading...'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              AssetPaths.durichLogo,
+              height: 100,
+              width: 100,
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'DURICH WAREHOUSE MANAGEMENT',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: AppColors.textPrimary,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
